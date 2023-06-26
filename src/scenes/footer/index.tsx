6f85f42c42c575ab3b1e8c2242d4b7cb534eb614
Sahil-khnn <sahil.khn338@gmail.com>
2023-06-26 16:34:17 +0500

    Update index.tsx

--- src/scenes/footer/index.tsx
+++ src/scenes/footer/index.tsx
@@ -2,6 +2,7 @@ import Logo from "@/assets/Logo.png";
 
 const Footer = () => {
   return (
+    <>
     <footer className="bg-primary-100 py-16">
       <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
         <div className="mt-16 basis-1/2 md:mt-0">
@@ -22,10 +23,11 @@ const Footer = () => {
         <div className="mt-16 basis-1/4 md:mt-0">
           <h4 className="font-bold">Contact Us</h4>
           <p className="my-5">Tempus metus mattis risus volutpat egestas.</p>
-          <p>(333)425-6825</p>
+          <p>111-222-333</p>
         </div>
       </div>
     </footer>
+    </>
   );
 };
 
